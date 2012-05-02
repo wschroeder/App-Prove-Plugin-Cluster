6748c5f70a9079a1067246f0836f7018ce516717
William Schroeder <wschroed@genome.wustl.edu>
2012-05-02 14:27:53 -0500

    Added features for performing tests in the same process as the slave.

--- lib/App/Prove/Plugin/ClusterLSF.pm
+++ lib/App/Prove/Plugin/ClusterLSF.pm
@@ -17,17 +17,28 @@ sub parse_lsf_options {
     local @ARGV = @args;
     Getopt::Long::Configure(qw(no_ignore_case bundling pass_through));
 
-    my ($lsf_queue, $lsf_resources, $lsf_startup, $lsf_teardown);
+    my ($lsf_queue, $lsf_resources, $lsf_startup, $lsf_teardown, $lsf_startup_in_process, $lsf_teardown_in_process, $lsf_test_in_process);
     GetOptions(
-        'lsf-queue=s'     => \$lsf_queue,
-        'lsf-resources=s' => \$lsf_resources,
-        'lsf-startup=s'   => \$lsf_startup,
-        'lsf-teardown=s'  => \$lsf_teardown,
+        'lsf-queue=s'               => \$lsf_queue,
+        'lsf-resources=s'           => \$lsf_resources,
+        'lsf-startup=s'             => \$lsf_startup,
+        'lsf-teardown=s'            => \$lsf_teardown,
+        'lsf-startup-in-process=s'  => \$lsf_startup_in_process,
+        'lsf-teardown-in-process=s' => \$lsf_teardown_in_process,
+        'lsf-test-in-process'       => \$lsf_test_in_process,
     ) or croak('Unable to parse parameters');
 
     $app->{argv} = [@ARGV];
 
-    return ($lsf_queue, $lsf_resources, $lsf_startup, $lsf_teardown);
+    if ($lsf_startup_in_process && !-e $lsf_startup_in_process) {
+        die "File specified in --lsf-startup-in-process ($lsf_startup_in_process) does not exist";
+    }
+
+    if ($lsf_teardown_in_process && !-e $lsf_teardown_in_process) {
+        die "File specified in --lsf-teardown-in-process ($lsf_teardown_in_process) does not exist";
+    }
+
+    return ($lsf_queue, $lsf_resources, $lsf_startup, $lsf_teardown, $lsf_startup_in_process, $lsf_teardown_in_process, $lsf_test_in_process);
 }
 
 sub load {
@@ -37,7 +48,11 @@ sub load {
     my $p   = shift;
     my $app = $p->{app_prove};
 
-    my ($lsf_queue, $lsf_resources, $lsf_startup, $lsf_teardown) = $class->parse_lsf_options($app);
+    my ($lsf_queue, $lsf_resources, $lsf_startup, $lsf_teardown, $lsf_startup_in_process, $lsf_teardown_in_process, $lsf_test_in_process) =
+        $class->parse_lsf_options($app);
+
+    my $includes  = $app->{includes};
+    my $test_args = $app->{test_args};
 
     $TAP::Harness::Master::DEFAULT_SLAVE_STARTUP_CALLBACK = sub {
         my ($self, $aggregate, @tests) = @_;
@@ -49,12 +64,17 @@ sub load {
                 ($lsf_queue     ? ('-q', $lsf_queue)     : ()),
                 ($lsf_resources ? ('-R', $lsf_resources) : ()),
                 'prove',
+                (@$includes               ? map {('-I', $_)} @$includes : ()),
                 '-PSlave',
                 '--master-host', hostname,
                 '--master-port', $TAP::Harness::Master::LISTEN_PORT,
-                ($lsf_startup   ? ('--lsf-startup',  $lsf_startup)  : ()),
-                ($lsf_teardown  ? ('--lsf-teardown', $lsf_teardown) : ()),
-                '--credentials', $self->{credentials}
+                ($lsf_startup             ? ('--lsf-startup',             $lsf_startup)  : ()),
+                ($lsf_teardown            ? ('--lsf-teardown',            $lsf_teardown) : ()),
+                ($lsf_startup_in_process  ? ('--lsf-startup-in-process',  $lsf_startup_in_process)  : ()),
+                ($lsf_teardown_in_process ? ('--lsf-teardown-in-process', $lsf_teardown_in_process) : ()),
+                ($lsf_test_in_process     ? '--lsf-test-in-process' : ()),
+                '--credentials', $self->{credentials},
+                (@$test_args              ? ('::', @$test_args) : ()),
             );
         }
     };
--- lib/App/Prove/Plugin/Slave.pm
+++ lib/App/Prove/Plugin/Slave.pm
@@ -16,13 +16,16 @@ sub parse_additional_options {
     local @ARGV = @args;
     Getopt::Long::Configure(qw(no_ignore_case bundling pass_through));
 
-    my ($master_host, $master_port, $credentials, $lsf_startup, $lsf_teardown);
+    my ($master_host, $master_port, $credentials, $lsf_startup, $lsf_teardown, $lsf_startup_in_process, $lsf_teardown_in_process, $lsf_test_in_process);
     GetOptions(
-        'master-host=s'  => \$master_host,
-        'master-port=s'  => \$master_port,
-        'credentials=s'  => \$credentials,
-        'lsf-startup=s'  => \$lsf_startup,
-        'lsf-teardown=s' => \$lsf_teardown,
+        'master-host=s'             => \$master_host,
+        'master-port=s'             => \$master_port,
+        'credentials=s'             => \$credentials,
+        'lsf-startup=s'             => \$lsf_startup,
+        'lsf-teardown=s'            => \$lsf_teardown,
+        'lsf-startup-in-process=s'  => \$lsf_startup_in_process,
+        'lsf-teardown-in-process=s' => \$lsf_teardown_in_process,
+        'lsf-test-in-process'       => \$lsf_test_in_process,
     ) or croak('Unable to parse parameters');
 
     $app->{argv} = [@ARGV];
@@ -37,30 +40,83 @@ sub parse_additional_options {
         die "Did not specify --credentials";
     }
 
-    return ($master_host, $master_port, $credentials, $lsf_startup, $lsf_teardown);
+    return ($master_host, $master_port, $credentials, $lsf_startup, $lsf_teardown, $lsf_startup_in_process, $lsf_teardown_in_process, $lsf_test_in_process);
 }
 
 our $TEARDOWN_CALLBACK = sub {};
+our $TEARDOWN_IN_PROCESS_CALLBACK = sub {};
 
 sub load {
     my ($class, $p) = @_;
     my $app  = $p->{app_prove};
-    my ($master_host, $master_port, $credentials, $lsf_startup, $lsf_teardown) = $class->parse_additional_options($app);
+    my ($master_host, $master_port, $credentials, $lsf_startup, $lsf_teardown, $lsf_startup_in_process, $lsf_teardown_in_process, $lsf_test_in_process) =
+        $class->parse_additional_options($app);
 
     local %SIG = %SIG;
+
     if ($lsf_teardown) {
         $TEARDOWN_CALLBACK = sub { system($lsf_teardown) };
-        for my $signal (qw(INT KILL ABRT STOP __DIE__)) {
-            $SIG{$signal} = $TEARDOWN_CALLBACK;
-        }
     }
+    if ($lsf_teardown_in_process) {
+        $TEARDOWN_IN_PROCESS_CALLBACK = sub {
+            $class->eval_perl_script_in_process($lsf_teardown_in_process);
+        };
+    }
+
+    for my $signal (qw(INT KILL ABRT STOP __DIE__)) {
+        $SIG{$signal} = sub {
+            $TEARDOWN_CALLBACK->();
+            $TEARDOWN_IN_PROCESS_CALLBACK->();
+        };
+    }
+
     if ($lsf_startup) {
         if (system($lsf_startup) || $?) {
             die "Startup failed";
         }
     }
 
-    $class->run_client($master_host, $master_port, $credentials);
+    if ($lsf_startup_in_process) {
+        $class->eval_perl_script_in_process($lsf_startup_in_process);
+    }
+
+    $class->run_client($master_host, $master_port, $credentials, $lsf_test_in_process, $app->{includes}, ($app->{test_args} || []));
+}
+
+sub includes {
+    my $class = shift;
+    my $includes = (shift) || [];
+    return map {($_ =~ m{^/}) ? $_ : $ENV{PWD} . "/$_"} @$includes;
+}
+
+sub eval_perl_script_in_process {
+    my $class    = shift;
+    my $job_info = shift;
+    my $args     = shift;
+
+    my $cwd = File::Spec->rel2abs('.');
+
+    #LSF: The code from here to exit is from  L<FCGI::Daemon> module.
+    local *CORE::GLOBAL::exit = sub { die 'notr3a11yeXit' };
+    local $0 = $job_info;    #fixes FindBin (in English $0 means $PROGRAM_NAME)
+    no strict;               # default for Perl5
+    {
+
+        package main;
+        local @ARGV = $args ? @$args : ();
+        do $0;               # do $0; could be enough for strict scripts
+        chdir($cwd);
+
+        if ($EVAL_ERROR) {
+            $EVAL_ERROR =~ s{\n+\z}{};
+            unless ( $EVAL_ERROR =~ m{^notr3a11yeXit} ) {
+                die $EVAL_ERROR;
+            }
+        }
+        elsif ($@) {
+            die $@;
+        }
+    }
 }
 
 sub get_test {
@@ -73,6 +129,7 @@ sub get_test {
     # Master prove is finished
     if (!defined($begin_line)) {
         $TEARDOWN_CALLBACK->();
+        $TEARDOWN_IN_PROCESS_CALLBACK->();
         exit(0);
     }
 
@@ -93,7 +150,7 @@ sub get_test {
 }
 
 sub run_client {
-    my ($class, $master_host, $master_port, $credentials) = @_;
+    my ($class, $master_host, $master_port, $credentials, $test_in_process, $includes, $test_args) = @_;
     my $socket;
     my $timeout = time + 10;
 
@@ -115,21 +172,57 @@ sub run_client {
     while (1) {
         my $test_info    = $class->get_test($socket, $credentials);
         my $test_source  = $test_info->{source};
-        my @switches     = @{$test_info->{switches}};
-        my $stdout       = IO::Handle->new;
-        my $stderr       = IO::Handle->new;
-        my $pid          = open3(undef, $stdout, $stderr, 'perl', @switches, $test_source);
+        my $switches     = $test_info->{switches} || [];
 
-        $socket->print('# Host: ' . hostname . "\n");
-
-        my @lines;
-        do {
-            my @ready = IO::Select->new($stdout, $stderr)->can_read;
-            @lines = grep {$_} map {$_->getline} @ready;
-            if (@lines) {
-                $socket->print(join('', grep {$_} @lines));
+        if ($test_in_process) {
+            # We need to fork because we want to create separate test plans within "the same process".
+            # Do not close the socket!  We need the same socket to Master open.
+            my $pid = fork();
+            if ($pid) {
+                waitpid( $pid, 0 );
             }
-        } while (@lines);
+            else {
+                eval {
+                    # Redirect STDERR and STDOUT
+                    local *STDERR = $socket;
+                    local *STDOUT = $socket;
+
+                    # Intercept all output from Test::More. Output all of them at once.
+                    require Test::More;
+                    my $builder = Test::More->builder;
+                    $builder->output($socket);
+                    $builder->failure_output($socket);
+                    $builder->todo_output($socket);
+
+                    # Set the environment to have our includes for tests
+                    if (@$includes) {
+                        $ENV{PERL5LIB} .= ':' . join ':', map {($_ =~ m{^/}) ? $_ : $ENV{PWD} . "/$_"} @$includes;
+                        push @INC, $class->includes($includes);
+                    }
+
+                    $class->eval_perl_script_in_process($test_source, $test_args);
+                };
+                if ($@) {
+                    $socket->print($@);
+                    exit(1);
+                }
+                exit(0);
+            }
+        }
+        else {
+            my $stdout = IO::Handle->new;
+            my $stderr = IO::Handle->new;
+            my $pid    = open3(undef, $stdout, $stderr, 'perl', @$switches, (map {('-I', $_)} $class->includes($includes)), $test_source, @$test_args);
+
+            my @lines;
+            do {
+                my @ready = IO::Select->new($stdout, $stderr)->can_read;
+                @lines = grep {$_} map {$_->getline} @ready;
+                if (@lines) {
+                    $socket->print(join('', grep {$_} @lines));
+                }
+            } while (@lines);
+        }
     }
 }
 
